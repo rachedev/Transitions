e0cedd331651ac45694b8fccb57ca95ff27a8f7e
Rachelle Lee <rachedeveloper@gmail.com>
2021-08-02 20:06:12 -0700

    Initial Commit

---
+++ Transitions/TransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionsApp.swift
+//  Transitions
+//
+//  Created by RachelleLee on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct TransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
